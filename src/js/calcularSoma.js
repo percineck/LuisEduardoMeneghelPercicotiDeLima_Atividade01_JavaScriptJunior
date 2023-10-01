8e039dae2944b2f10ab7bcea08309631ceea4d00
luis.percicoti@gmail.com <luis.percicoti@gmail.com>
2023-10-01 19:37:14 -0300

    first commit

---
+++ src/js/calcularSoma.js
@@ -0,0 +1,15 @@
+function calcularSoma() {
+    // Declaração de variáveis
+    let soma = 0;
+    let i = 1;
+  
+    // Loop para calcular a soma
+    while (i <= 100) {
+      soma += i;
+      i++;
+    }
+  
+    // Exibe o resultado
+    window.alert("A soma é: " + soma);
+  }
+  
